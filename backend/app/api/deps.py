b2a29f535a3458239f673b725908ae2ccb8c3244
guilhermeUpToTask <guilherme256teretelteltel@gmail.com>
2025-05-25 11:23:57 -0300

    fix[auth]: fixed uuid compatibilite in jwt decode, we convert the str id back to uuid for testing

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 from collections.abc import Generator
-
+import uuid
 import jwt
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
@@ -25,7 +25,7 @@ def get_db() -> Generator[Session, None, None]:
 SessionDep = Annotated[Session, Depends(get_db)]
 TokenDep = Annotated[str, Depends(reusable_oauth2)]
 
-def get_current_user(session: SessionDep, token:TokenDep) -> User:
+def get_current_user(session: SessionDep, token: TokenDep) -> User:
     try:
         payload = jwt.decode(
             token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
@@ -34,14 +34,22 @@ def get_current_user(session: SessionDep, token:TokenDep) -> User:
     except (InvalidTokenError, ValidationError):
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
-            detail="Could not validate credientials",
+            detail="Could not validate credentials",
         )
         
-    user = session.get(User, token_data.sub)
+    try:
+        user_id = uuid.UUID(token_data.sub)
+    except (ValueError, TypeError):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Invalid token payload.",
+        )
+    user = session.get(User, user_id)   
+
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User Not Found")
+        raise HTTPException(status_code=404, detail="User not found")
     if not user.is_active:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Inactive user")
+        raise HTTPException(status_code=400, detail="Inactive user")
     return user
 
 CurrentUser = Annotated[User, Depends(get_current_user)]
@@ -52,5 +60,3 @@ def get_current_admin_user(current_user: CurrentUser) -> User:
             status_code=403, detail="The user doesn't have enough privileges"
         )
     return current_user
-
-    
